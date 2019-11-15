51e62b2860021d65080f5d5dc81082273a8f862e
zhangyunteng <zhangyunteng@guazi.com>
2019-11-15 16:24:21 +0800

    master

--- src/main/java/yt/follow/log/controller/HttpRequestController.java
+++ src/main/java/yt/follow/log/controller/HttpRequestController.java
@@ -2,10 +2,15 @@ package yt.follow.log.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import yt.follow.log.service.IFunctionService;
+
+import java.util.Set;
 
 /**
  * @author yunteng
@@ -20,5 +25,26 @@ public class HttpRequestController {
 	public void getHttpRequest(){
 		restTemplate.getForObject("http://127.0.0.1:9069/get-all-user-info?id=3",Object.class);
 		log.info("getHttpRequest====success");
+	}
+
+
+	/**
+	 * 获取接口的所有实现类
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Reflections reflections = new Reflections("yt.follow.log");
+		Set<Class<? extends IFunctionService>> set = reflections.getSubTypesOf(IFunctionService.class);
+		if(CollectionUtils.isNotEmpty(set)) {
+			set.forEach(one -> {
+				try {
+					IFunctionService functionService = one.newInstance();
+					functionService.function1();
+					functionService.function3();
+				} catch (InstantiationException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			});
+		}
 	}
 }
